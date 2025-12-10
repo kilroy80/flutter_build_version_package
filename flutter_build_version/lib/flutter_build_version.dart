bfc15112728ce044f450ec750f1b758ed88e2395
kilroy80 <kilroy80@gmail.com>
2025-12-10 16:02:19 +0900

    feat: Update example code

--- flutter_build_version/lib/flutter_build_version.dart
+++ flutter_build_version/lib/flutter_build_version.dart
@@ -1,6 +1,30 @@
 import 'package:meta/meta.dart';
 
-/// const [GenerateBuildVersion]
+/// Example usage of @GenerateBuildVersion()
+///
+/// ```dart
+/// import 'package:flutter_build_version/flutter_build_version.dart';
+/// 
+/// part 'app_version.gb.dart';
+///
+/// @GenerateBuildVersion()
+/// abstract class _AppVersion {}
+/// ```
+///
+/// dart run or watch [build_runner]
+///
+/// ```bash
+/// dart run build_runner build --delete-conflicting-outputs
+/// ```
+///
+/// After running, generate file use
+///
+/// ```dart
+/// debugPrint(AppVersion.flutter); // e.g. "3.38.0"
+/// debugPrint(AppVersion.dart);    // e.g. "3.10.1"
+/// ```
+///
+/// const [GenerateBuildVersion] Annotation
 @immutable
 class GenerateBuildVersion {
   const GenerateBuildVersion();
